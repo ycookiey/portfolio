4c64aca8f77bb9a5a05f77aaf9250e5297130a71
ycookiey <70356861+ycookiey@users.noreply.github.com>
2025-03-27 12:44:47 +0900

    feat: Vue Routerを追加し、アプリケーションのルーティングを設定

---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '@/App.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: App
+    }
+  ],
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth'
+      }
+    }
+    return { top: 0 }
+  }
+})
+
+export default router
